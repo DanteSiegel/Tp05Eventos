74e1d652a1c025dff1fe4a0a7ba423732e85b212
Android Studio <devandroid@ort>
2024-05-31 11:18:51 -0300

    base lista

--- index.js
+++ index.js
@@ -1,18 +1,15 @@
-import express from "express";
-import cors from "cors";
-import ProvinceRouter from "./src/controllers/province-controller.js"
-const app = express();
-const port = 3000; // El puerto 3000 (http://localhost:3000)
-// Agrego los Middlewares
-app.use(cors()); // Middleware de CORS.
-app.use(express.json()); // Middleware para parsear y comprender JSON.
-//
-// Endpoints (todos los Routers)
-//
-app.use("/api/province", ProvinceRouter);
-//
-// Inicio el Server y lo pongo a escuchar.
-//
-app.listen(port, () => {
-console.log(`Example app listening on port ${port}`)
-})
+import config from './src/configs/db-config.js'
+import pkg from 'pg'
+const { Client } = pkg;
+
+// https: //node-postgres.com/apis/client
+const client = new Client(config);
+await client.connect();
+
+let sql = `SELECT * from provinces` ; // "... limit 5
+let result = await client.query(sql);
+
+await client.end();
+
+// 'rows' es un array. rows[0] el 1%er registro.
+console.log(result.rows);
